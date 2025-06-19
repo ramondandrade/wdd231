e51ab313e9dfa2e9ab9e910ee529b3ab90b4e47e
Ramon Andrade <tudonetrn@gmail.com>
2025-06-19 12:01:39 +0200

    week 6 Final - 3

--- final/scripts/home.js
+++ final/scripts/home.js
@@ -1,15 +1,18 @@
 
 
 async function setBackgroundImageFromAPI() {
+    const banner_home = document.getElementById('hero_home');
     try {
-        const banner_home = document.getElementById('hero_home');
         const response = await fetch('https://foodish-api.com/api/');
         const data = await response.json();
         if (data && data.image) {
             banner_home.style.backgroundImage = `url('${data.image}')`;
+        }else{
+            banner_home.style.backgroundImage = `url('images/banner.webp')`;
         }
     } catch (error) {
         console.error('Error fetching image:', error);
+        banner_home.style.backgroundImage = `url('images/banner.webp')`;
     }
 }
 
